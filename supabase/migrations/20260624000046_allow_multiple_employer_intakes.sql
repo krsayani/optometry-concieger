e6e1791a0bf93b6cc973768cbe69d5b61ae5c4f1
Muhammad Abu Bakar <abubakar1house@gmail.com>
2026-07-20 21:49:13 +0500

    Delete supabase directory

--- supabase/migrations/20260624000046_allow_multiple_employer_intakes.sql
+++
@@ -1,8 +0,0 @@
--- Allow employers to submit multiple hiring requests
--- Remove the unique constraint on user_id for employer_intake_responses
-ALTER TABLE public.employer_intake_responses DROP CONSTRAINT IF EXISTS employer_intake_responses_user_id_key;
-
--- Remove the unique constraint on email for employer_intake_responses
-ALTER TABLE public.employer_intake_responses DROP CONSTRAINT IF EXISTS employer_intake_responses_email_key;
-
--- Note: We keep the unique constraint for ODs as they should only have one career profile.
--- supabase/migrations/20260703000000_add_license_states.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE public.od_intake_responses ADD COLUMN license_states text;
